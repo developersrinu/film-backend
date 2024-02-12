3838182af56fc4e7526cac84316531253402f716
developersrinu <orsusrinu606@gmail.com>
2024-02-12 15:11:33 +0530

    ok

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // const mongoose = require('mongoose');
 // const db = require('./db/mongodb');
 // require('dotenv').config();
+// require('dotenv').config();
 
 
 const express = require('express');
